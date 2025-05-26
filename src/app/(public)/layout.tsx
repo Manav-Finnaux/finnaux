ef5abeb63d45936d53b1639f7cf5304984c5afac
sakshamjain-Finnaux <sakshamjain@finnaux.co.in>
2025-05-26 16:53:40 +0530

    kuch to kia

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/composed/Footer";
 import Navbar from "@/components/composed/nav";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";̀
+import { useEffect } from "react";
 
 export default function RootLayout({
   children,
